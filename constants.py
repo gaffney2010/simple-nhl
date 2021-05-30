54016295ea95dc081cb41c62174147a608f80d05
T.J. Gaffney <gaffney.tj@gmail.com>
2021-05-30 17:13:45 -0500

    Make models file

---
+++ constants.py
@@ -0,0 +1,38 @@
+"""Some constants that will be shared potentially across files."""
+
+# An allowlist of teams.  Should match to https://www.cbssports.com/nhl/teams/
+#  short names in URLs.  We exclude Golden Knights in order to have
+#  apples-to-apples comparison with earlier seasons.
+ALL_TEAMS = [
+  "CAR",
+  "CHI",
+  "CLB",
+  "DAL",
+  "DET",
+  "FLA",
+  "NSH",
+  "TB",
+  "BOS",
+  "BUF",
+  "NYI",
+  "NYR",
+  "NJ",
+  "PHI",
+  "PIT",
+  "WAS",
+  "CGY",
+  "EDM",
+  "MON",
+  "OTT",
+  "TOR",
+  "VAN",
+  "WPG",
+  "ANA",
+  "ARI",
+  "COL",
+  "LA",
+  "MIN",
+  "SJ",
+  "STL",
+  "LV",
+]
