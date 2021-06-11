2c9a7203d030e8c3bea630bc248fb8e5c61c83e4
T.J. Gaffney <gaffney.tj@gmail.com>
2021-06-11 01:03:19 -0500

    Do some model measuring

--- blog_posts/blog_post_2.py
+++ blog_posts/blog_post_2.py
@@ -5,14 +5,16 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+import numpy as np
+import pandas as pd
 import seaborn as sns
+import statsmodels.api as sm
 
 sns.set_theme()
 
 import game_data
 import models
 from shared_types import *
-
 
 # Graph for Interaction model
 data_18 = game_data.make_dataset(Season(2018))
@@ -25,6 +27,7 @@ def get_shots_on_goal(game: Game) -> Optional[models.AwayHomeTarget]:
     if data is None:
         return None
     return (data.away_att, data.home_att)
+
 
 for model in [
     models.InteractionModel(get_shots_on_goal),
@@ -46,7 +49,8 @@ def goal_percentage(game: Game) -> Optional[models.AwayHomeTarget]:
         return None
     if data.away_att == 0 or data.home_att == 0:
         return None
-    return (data.away_score/data.away_att, data.home_score/data.home_att)
+    return (data.away_score / data.away_att, data.home_score / data.home_att)
+
 
 for model in [
     models.InteractionModel(goal_percentage),
@@ -71,7 +75,7 @@ class ShotsWithGoalPerc(models.PointsModel):
         self._goal_perc_model = models.DefenseOnlyModel(goal_percentage)
         super().__init__()
 
-    def fit(self, train_set: List[Game]) -> None:
+    def fit(self, train_set: TrainTest) -> None:
         self._shot_model.fit(train_set)
         self._goal_perc_model.fit(train_set)
 
@@ -83,6 +87,69 @@ class ShotsWithGoalPerc(models.PointsModel):
 
 
 logging.info("ShotsWithGoalPerc Model")
-model = ShotsWithGoalPerc()
-model.fit(data_18)
-logging.info(model.score(data_18))
+shots_w_gp = ShotsWithGoalPerc()
+shots_w_gp.fit(data_18)
+logging.info(shots_w_gp.score(data_18))
+
+logging.info("")
+logging.info("")
+
+benchmark = models.OffenseOnlyModel()
+benchmark.fit(data_18)
+
+
+
+
+
+for model in (benchmark, shots_w_gp):
+    logging.info("===================")
+    logging.info(model)
+
+    score_diff, away_won, neg_home_ad = list(), list(), list()
+    for game in data_18.train:
+        data = game_data.load_game_data(game)
+        if data.away_score == data.home_score:
+            # Tie.  Should never happen?
+            continue
+
+        pred_away, pred_home = model.predict(game)
+        score_diff.append(pred_away - pred_home)
+        away_won.append(data.away_score > data.home_score)  # Actual
+        neg_home_ad.append(1)
+    df = pd.DataFrame({"away_won": away_won, "score_diff": score_diff,
+                       "neg_home_ad": neg_home_ad})
+    log_reg_ = sm.Logit(df["away_won"], df[["score_diff", "neg_home_ad"]])
+    log_reg = log_reg_.fit()
+    # logging.info(results.summary())
+
+    logging.info("")
+
+    # Calculate the win percentage - how often the model is right.
+    win_num, win_den = 0, 0
+    # Calculate the log-likelihood.
+    log_likelihood = 0.0
+
+    for game in data_18.test:
+        # String the two predictions together
+        data = game_data.load_game_data(game)
+        if data.away_score == data.home_score:
+            # Tie.  Should never happen?
+            continue
+
+        pred_away, pred_home = model.predict(game)
+        pred = log_reg.predict([pred_away - pred_home, 1])[0]
+        act = data.away_score > data.home_score
+
+        win_den += 1
+        if (pred > 0.5 and act) or (pred < 0.5 and not act):
+            win_num += 1
+
+        if act:
+            log_likelihood += np.log(pred)
+        else:
+            log_likelihood += np.log(1 - pred)
+
+    logging.info("Win perc")
+    logging.info(win_num / win_den)
+    logging.info("Log-likelihood")
+    logging.info(log_likelihood)
